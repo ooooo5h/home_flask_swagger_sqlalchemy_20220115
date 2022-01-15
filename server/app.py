54b197aef8cdde8e12c9db94f03d338ce31b4e84
전은형 <ooooo5h91@gmail.com>
2022-01-15 22:45:24 +0900

    create_app시, 어떤 환경으로 돌릴건지 받아서 적용

--- server/app.py
+++ server/app.py
@@ -1,6 +1,9 @@
+from http import server
 from flask import Flask
 
-def create_app():
+def create_app(config_name):
     app = Flask(__name__)
+    
+    app.config.from_object(f'server.config.{config_name}')
     
     return app
