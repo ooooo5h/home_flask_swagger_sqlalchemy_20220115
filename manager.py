93efe0e8162fcaf6a4197d11da518b2f5fc19152
전은형 <ooooo5h91@gmail.com>
2022-01-15 23:01:20 +0900

    python manager.py "키워드"에 따라 다른 환경 실행 셋팅
    flask_script/ Manager 클래스 기능 활용

--- manager.py
+++ manager.py
@@ -1,4 +1,18 @@
 from server import create_app
+from flask_script import Manager
 
 app = create_app('ProductionConfig')
-app.run(host='0.0.0.0')
+
+manager = Manager(app)
+
+@manager.command
+def debug():
+    app.config.from_object('server.config.DebugConfig')
+    app.run(host='0.0.0.0')
+    
+@manager.command
+def runserver():
+    app.run(host='0.0.0.0')
+    
+if __name__ == '__main__':
+    manager.run()
