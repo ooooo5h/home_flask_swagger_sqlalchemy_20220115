b4ffa90be2980c90237081c56c7d0009003f2ced
전은형 <ooooo5h91@gmail.com>
2022-01-15 23:24:19 +0900

    우리가 만든 클래스를 Api로 활용하도록 도와주는 세팅을 진행함

--- server/app.py
+++ server/app.py
@@ -1,9 +1,12 @@
-from http import server
 from flask import Flask
+from flask_restful_swagger_2 import Api
 
 def create_app(config_name):
     app = Flask(__name__)
     
     app.config.from_object(f'server.config.{config_name}')
+    
+    api = Api(app, api_spec_url='/api/spec', title='my_server spec', api_version='0.1', catch_all_404s=True)
+    
     
     return app
