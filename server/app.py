487bc3f95c2988d69e3715af00a88921567a0689
전은형 <ooooo5h91@gmail.com>
2022-01-15 23:36:46 +0900

    swagger 문서 페이지 등록

--- server/app.py
+++ server/app.py
@@ -1,5 +1,7 @@
+from distutils.command.config import config
 from flask import Flask
 from flask_restful_swagger_2 import Api
+from flask_swagger_ui import get_swaggerui_blueprint
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -15,6 +17,10 @@ def create_app(config_name):
     
     api.add_resource(User, '/user')
     api.add_resource(Lecture, '/lecture')
+    
+    
+    swagger_ui = get_swaggerui_blueprint('/api/docs', '/api/spec.json', config={'app_name' : 'my sns service'})
+    app.register_blueprint(swagger_ui, url_prefix='/api/docs')
     
     
     return app
