50b81e0b947dea70c9b332f52b5ca0b35144e0ce
전은형 <ooooo5h91@gmail.com>
2022-01-17 21:01:19 +0900

    DB 연결 정보 명시

--- server/app.py
+++ server/app.py
@@ -1,15 +1,21 @@
-from distutils.command.config import config
 from flask import Flask
 from flask_restful_swagger_2 import Api
 from flask_swagger_ui import get_swaggerui_blueprint
+# DB담당하는 라이브러리를 import
+from flask_sqlalchemy import SQLAlchemy
+
+# DB연결을 전담하는 변수를 만들고, import로 찾아다 쓸 수 있게 세팅
+db = SQLAlchemy()
 
 def create_app(config_name):
     app = Flask(__name__)
     
     app.config.from_object(f'server.config.{config_name}')
+       
+    #SQL Alchemy 세팅 진행 => 플라스크에 해둔 환경설정값을 불러다가 활용
+    db.init_app(app)
     
     api = Api(app, api_spec_url='/api/spec', title='my_server spec', api_version='0.1', catch_all_404s=True)
-    
     
 
     from server.api.user import User
--- server/config.py
+++ server/config.py
@@ -6,6 +6,11 @@ class Config(object):
     DEBUG = False
     TESTING = False
     
+    # SQLAlchemy가 접속할 DB 연결 정보(URI)
+    # SQLAlchemy 라이브러리가, 어떤 변수를 끌어다 쓸 지도 미리 지정되어있어서 변수이름 바꾸면 안됨!!
+    # SQLALCHEMY_DATABASE_URI = "mysql+pymysql://아이디:비밀번호@DB호스트주소/논리DB이름"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://admin:admin123"+\
+                            "@my-first-db2.ckcb9pt3t4a9.ap-northeast-2.rds.amazonaws.com/my_sns"
     
 class ProductionConfig(Config):
     pass
