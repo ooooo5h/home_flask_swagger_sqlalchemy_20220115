d2769ee79c077eeed5fb63935ef4ebf9ab854ac3
전은형 <ooooo5h91@gmail.com>
2022-01-17 21:34:26 +0900

    가입 완료한 데이터 리턴
    db.session.commit() 이후에는 id값도 자동으로 세팅됨

--- server/api/user/user.py
+++ server/api/user/user.py
@@ -165,5 +165,10 @@ class User(Resource):
         db.session.commit()
         
         return{
-            '임시' : '회원가입'
+            'code' : 200,
+            'message' : '회원가입 성공',
+            'data' : {
+                'user' : new_user.get_data_object()
+            }
         }
+        
