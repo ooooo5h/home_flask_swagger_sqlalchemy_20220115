8bdfe341a5bea55e57055b14e2a72d74460f86cc
전은형 <ooooo5h91@gmail.com>
2022-01-17 23:05:19 +0900

    swagger 문서 작업

--- server/api/feed/feed.py
+++ server/api/feed/feed.py
@@ -1,8 +1,45 @@
 from flask_restful import Resource
+from flask_restful_swagger_2 import swagger
 
 class Feed(Resource):
     
+    @swagger.doc({
+        'tags' : ['feed'],
+        'description' : '게시글 등록하기',
+        'parameters' : [
+            {
+                'name' : 'user_id',
+                'description' : '어느 사용자가 쓴건지',
+                'in' : 'formData',
+                'type' : 'integer',  
+                'required' : True
+            },   
+            {
+                'name' : 'lecture_id',
+                'description' : '어느 강의에 대해 쓴건지',
+                'in' : 'formData',
+                'type' : 'integer',  
+                'required' : True
+            },        
+            {
+                'name' : 'content',
+                'description' : '게시글 내용',
+                'in' : 'formData',
+                'type' : 'string',  
+                'required' : True
+            }
+        ],
+        'responses' : {
+            '200' : {
+                'description' : '게시글 등록 성공',
+            },
+            '400' : {
+                'description' : '게시글 등록 실패',
+            }
+        }
+    })    
     def post(self):
+        """게시글 등록하기"""
         return {
             'code' : '임시 게시글 등록'
         }
