15cc4d1b7e9c57e5a7280cd27b42da62430fa6e9
전은형 <ooooo5h91@gmail.com>
2022-01-17 23:24:52 +0900

    연결된 Users객체 활용 -> 작성자 정보도 같이 리턴

--- server/model/feeds.py
+++ server/model/feeds.py
@@ -20,6 +20,7 @@ class Feeds(db.Model):
             'lecture_id' : self.lecture_id,
             'content' : self.content,
             'created_at' : str(self.created_at),
+            'writter' : self.writter.get_data_object(),
         }
         
         # 이 글의 작성자가 누군지 알 수 있다면, json을 만들 때마다 자동으로 첨부되면 편하겠다
