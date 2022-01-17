56012fece72fd2879908a5c977cd4cbd0a7c0f84
전은형 <ooooo5h91@gmail.com>
2022-01-17 20:36:15 +0900

    로그인 기능에 사용할 파라미터를 명시
    required=True : 첨부앙ㄴ하면 에러를 리턴

--- server/api/user/user.py
+++ server/api/user/user.py
@@ -1,5 +1,11 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 from flask_restful_swagger_2 import swagger
+
+# post메쏘드에서 사용할 파라미터
+post_parser = reqparse.RequestParser()
+post_parser.add_argument('email', type=str, required=True, location='form')  # 파라미터 이름/데이터타입/필수여부/첨부된 곳 명시
+post_parser.add_argument('password', type=str, required=True, location='form')
+
 
 class User(Resource):
     
@@ -27,7 +33,7 @@ class User(Resource):
   
     @swagger.doc({
         'tags' : ['user'],
-        'description' : '로그인 조회',
+        'description' : '로그인',
         'parameters' : [
             
         ],
