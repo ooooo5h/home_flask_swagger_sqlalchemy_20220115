97ac360634d81edc1c791719913b211c87aae656
전은형 <ooooo5h91@gmail.com>
2022-01-18 20:52:06 +0900

    실제 응답 처리
    강의 정보에 있는 강사정보 첨부하는 기능도 활용

--- server/api/lecture/lecture_detail.py
+++ server/api/lecture/lecture_detail.py
@@ -1,8 +1,23 @@
 from flask_restful import Resource
 
+from server.model import Lectures
+
 class LectureDetail(Resource):
     
     def get(self, lecture_id):
+        
+        lecture_row = Lectures.query.filter(Lectures.id == lecture_id).first()
+        
+        if lecture_row is None:
+            return{
+                'code' : 400,
+                'message' : '해당 강의는 존재하지 않습니다.'
+            }, 400
+        
         return {
-            'code' : f"{lecture_id}번 강의 상세조회"
+            'code' : 200,
+            'message' : '강의 상세 조회',
+            'data' : {
+                'lecture' : lecture_row.get_data_object(need_teacher_info=True)
+            }
         }
