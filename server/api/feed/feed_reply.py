3997830a750ca8eba82b8e8319df47edcbdf1997
전은형 <ooooo5h91@gmail.com>
2022-01-19 22:10:11 +0900

    댓글 삭제 기능 구현

--- server/api/feed/feed_reply.py
+++ server/api/feed/feed_reply.py
@@ -10,10 +10,13 @@ from server.api.utils import token_required
 post_parser = reqparse.RequestParser()
 post_parser.add_argument('content', type=str, required=True, location='form')
 
-
 put_parser = reqparse.RequestParser()
 put_parser.add_argument('feed_reply_id', type=int, required=True, location='form')
 put_parser.add_argument('content', type=str, required=True, location='form')
+
+delete_parser = reqparse.RequestParser()
+delete_parser.add_argument('feed_reply_id', type=int, required=True, location='args')
+
 
 class FeedReply(Resource):
     
@@ -126,8 +129,7 @@ class FeedReply(Resource):
         user = g.user     
         
         # 내가 쓴 댓글이 맞는지 확인하기
-        reply = FeedReplies.query.filter(FeedReplies.id == args['feed_reply_id']).first()
-        
+        reply = FeedReplies.query.filter(FeedReplies.id == args['feed_reply_id']).first()     
         if reply.user_id != user.id:
             return {
                 'code' : 400,
@@ -141,5 +143,62 @@ class FeedReply(Resource):
 
         return {
         'code' : 200,
-        'message' : '임시 : 댓글 수정 성공'
+        'message' : '댓글 수정 성공'
+    }
+     
+        
+    @swagger.doc({
+        'tags' : ['feed/reply'],
+        'description' : '달아둔 댓글 삭제하기',
+        'parameters' : [
+            {
+                'name' : 'X-Http-Token',
+                'description' : '사용자 토큰',
+                'in' : 'header',
+                'type' : 'string',  
+                'required' : True
+            },   
+            {
+                'name' : 'feed_id',
+                'description' : '몇번 게시글에 포함된 댓글을 수정할지',
+                'in' : 'path',
+                'type' : 'integer',  
+                'required' : True
+            },        
+            {
+                'name' : 'feed_reply_id',
+                'description' : '몇번 댓글을 삭제할지',
+                'in' : 'query',
+                'type' : 'integer',  
+                'required' : True
+            },   
+        ],
+        'responses' : {
+            '200' : {
+                'description' : '삭제 성공',
+            }
+        }
+    })    
+    @token_required
+    def delete(self, feed_id):
+        """달아둔 댓글 삭제하기"""       
+        
+        args = delete_parser.parse_args()      
+        user = g.user     
+        
+        # 내가 쓴 댓글이 맞는지 확인하기
+        reply = FeedReplies.query.filter(FeedReplies.id == args['feed_reply_id']).first()
+        
+        if reply.user_id != user.id:
+            return {
+                'code' : 400,
+                'message' : '본인이 쓴 댓글만 삭제할 수 있습니다.'
+            }, 400
+
+        db.session.delete(reply)
+        db.session.commit()         
+
+        return {
+        'code' : 200,
+        'message' : '댓글 삭제 성공'
     }
