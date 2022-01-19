08d50a989bd96da45824eac060837e7751f40c82
전은형 <ooooo5h91@gmail.com>
2022-01-19 21:14:19 +0900

    댓글과 사용자간의 관계 설정
    정석아닌 방법으로 작성함.

--- server/model/feed_replies.py
+++ server/model/feed_replies.py
@@ -9,6 +9,8 @@ class FeedReplies(db.Model):
     content = db.Column(db.TEXT, nullable=False)  
     created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
 
+    # 정석이 아닌 방법 : 자식의 입장에서 부모가 누군지 변수로 들자
+    writer = db.relationship('Users')
     
     def get_data_object(self):
         data = {
@@ -17,6 +19,7 @@ class FeedReplies(db.Model):
             'user_id' : self.user_id,
             'content' : self.content,
             'created_at' : str(self.created_at),
+            'writer' : self.writer.get_data_object()
         }
               
         return data
