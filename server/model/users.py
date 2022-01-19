6cb08ce9cca20d35baf98b1ad4e7a72a2238fc0f
전은형 <ooooo5h91@gmail.com>
2022-01-19 23:05:09 +0900

    사용자 암호컬럼 변경
    비번 원문이 아니라, 암호화된 비번을 담기위한 컬럼(32)

--- server/model/users.py
+++ server/model/users.py
@@ -10,7 +10,7 @@ class Users(db.Model):
     # 2 : 어떤 변수 / 어떤 컬럼 연결 명시 => 변수이름 = 컬럼 이름
     id = db.Column(db.Integer, primary_key=True)  # id라는 컬럼은 Int형이고, 기본키라고 명시
     email = db.Column(db.String(50), nullable=False, default='이메일 미입력')   # email컬럼은 50자 문구에 null불가하고, 기본값이 있다고 명시
-    password = db.Column(db.String(50), nullable=False)
+    password_hashed = db.Column(db.String(32), nullable=False)
     name = db.Column(db.String(20), nullable=False)
     phone = db.Column(db.String(15))  # nullable의 기본값은 null허용
     birth_year = db.Column(db.Integer, nullable=False, default=1995)
