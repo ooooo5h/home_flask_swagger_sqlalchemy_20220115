7c09842d4f0a7d1b017d3dbd7647ce10160b52fc
전은형 <ooooo5h91@gmail.com>
2022-01-20 20:47:30 +0900

    검색 결과의 갯수는 len보다 count가 효율적

--- server/api/admin/dashboard.py
+++ server/api/admin/dashboard.py
@@ -7,13 +7,13 @@ class DashBoard(Resource):
     def get(self):
         
         # 탈퇴하지 않은 회원 수? => SELECT / users 테이블 활용 => Users 모델 import
-        users = Users.query.filter(Users.email != 'retired').all()
-        
+        # first()는 한 줄 / all()은 목록 / count() 는 검색된 갯수
+        users_count = Users.query.filter(Users.email != 'retired').count()
         
         return{
             'code' : 200,
             'message' : '관리자용 각종 통계 api',
             'data' : {
-                'live_user_count' : len(users)
+                'live_user_count' : users_count
             }
         }
