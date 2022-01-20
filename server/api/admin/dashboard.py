6d401d4e4039779279f9f137fc40904472462f53
전은형 <ooooo5h91@gmail.com>
2022-01-20 21:03:39 +0900

    자바 강의에 대한 강의로 합계

--- server/api/admin/dashboard.py
+++ server/api/admin/dashboard.py
@@ -1,5 +1,6 @@
 from flask_restful import Resource
 from server.model import Users, Lectures, LectureUser
+from server import db
 
 class DashBoard(Resource):
     
@@ -10,15 +11,19 @@ class DashBoard(Resource):
         users_count = Users.query.filter(Users.email != 'retired').count()
         
         
-        # 연습 : 자바 강의를 듣는 사람들의 정보 => JOIN 어떻게 할꺼야..?
+        # 연습 : 자바 강의의 매출 총액이 궁금해=> JOIN 어떻게 할꺼야..?
+        # query(SELECT문의 컬럼 선택처럼 여러 항목 가능)
+        # db.func.집계함수(컬럼) => 집계함수 동작
         
-        java_user_list = Users.query\
+        # filter 나열 => JOIN과 ON을 한번에 명시
+        # filter 나열 2 => 마지막 filter는 JOIN이 끝난 후, WHERE절처럼 실제 필터 조건을 적으면 됨
+        java_amount = db.session.query(Lectures.title, db.func.sum(Lectures.fee))\
+            .filter(Lectures.id == LectureUser.lecture_id)\
             .filter(LectureUser.user_id == Users.id)\
-            .filter(LectureUser.lecture_id == Lectures.id)\
             .filter(Lectures.title == '자바')\
             .all()
-            
-        print(java_user_list)
+        
+        print(java_amount)
         
         return{
             'code' : 200,
